387ce3b4ece03bba4af052db62c3681e6c24757d
Cally N <64858908+callyjoyous@users.noreply.github.com>
2023-09-08 11:24:55 -0400

    Create TipCalculator.py

---
+++ TipCalculator.py
@@ -0,0 +1,10 @@
+print('Welcome to the tip calculator.')
+bill = float(input('What was the total bill? $'))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people will be splitting this bill? "))
+
+bill_with_tip = tip / 100 * bill + bill 
+
+bill_split = bill_with_tip / people
+
+print("The bill split " + str(people) + "ways is: $" + str(bill_split))
